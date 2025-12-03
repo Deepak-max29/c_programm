bd714771239d0fd1218ac9d9874f2da1d1d780da
Deepak-max29 <kancharlajayadeepak@gmail.com>
2025-12-03 13:32:45 +0530

    Add files via upload

---
+++ hellol.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void printhello(void);
+void chandu(void);
+int main(void){
+    void printhello(void);
+    void chandu(void);
+    return 0;
+}
+void printhello(void){
+    printf("Hello\n");
+}
+void chandu(void){
+    printf("Chandu\n");
+}
