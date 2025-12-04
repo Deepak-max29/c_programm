e242a4fe3758140e915c9d6b81e92aa3be63a69e
Deepak-max29 <kancharlajayadeepak@gmail.com>
2025-12-04 10:02:11 +0530

    Add files via upload

---
+++ sumdiffdivision.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	float a,b;
+	printf("enter the value of 1st no. :");
+	scanf("%f",&a);
+	printf("enter the value of 2nd no. :");
+	scanf("%f",&b);
+	printf("the sum of %f and %f is %f\n",a,b,a+b);
+	printf("the diffrence between %f and %f is %f\n",a,b,a-b);
+	printf("the product of %f and %f is %f\n",a,b,a*b);
+	printf("the quotient when %f is divided by %f is %f\n",a,b,a/b);
+	return 0;
+}
