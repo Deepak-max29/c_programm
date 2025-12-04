3b439b57cfb634b1232ba2cae14028cb7f018e6b
Deepak-max29 <kancharlajayadeepak@gmail.com>
2025-12-04 09:59:37 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter a number : ");
+	scanf("%d",&a);
+	if (50>=a && a>=10)
+	{printf("the no. is in range.");
+	
+	}
+else{printf("the no.is not in range");
+}
+return 0;
+}
