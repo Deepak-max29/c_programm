abcda9a94cee3104ad31cbab1eeb15290a2fd1f8
Deepak-max29 <kancharlajayadeepak@gmail.com>
2025-12-04 10:19:03 +0530

    Add files via upload

---
+++ digits_sum_caluculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char s[1000];
+    int i,sum=0;
+    printf("Enter a number : ");
+    scanf("%s",s);
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]>='0' && s[i]<='9'){
+            sum+=s[i]-'0';
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
